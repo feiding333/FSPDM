9929c78effc73f4f085f16ade4ec82b8e3e6e9a5
Fei Ding <feiding@tamu.edu>
2019-11-20 20:40:16 -0600

    Write the objective function.

--- src/FSPDM.cpp
+++ src/FSPDM.cpp
@@ -27,3 +27,58 @@ void ObjGradient::store_intermediateData(){
     }
   }
 }
+
+// Write the objective function.
+double ObjGradient::objfunc(){
+  double s =0;
+  mat tmpFn;
+  mat tmpxn;
+  mat IdentityMr = eye<mat>(r,r);
+  mat tmp_cholM;
+  vec tmpgn;
+  vec tmpdiagFn;
+  vec SA;
+  vec SA_sqrt;
+
+  if(sigma20 != 100000){
+    for (int i = 0; i < N ; i++) {
+      tmp_cholM = IdentityMr + pow(sigma2,-1) * (C.at(i).t()*BtB.at(i)*C.at(i));
+      //std::cout << "pow(sigma2,-1)="<< pow(sigma2,-1)  << std::endl;
+      tmpgn = C.at(i).t()*(Btx.at(i) - BtH.at(i)*theta);
+
+      // get F_n
+      tmpFn = chol(tmp_cholM, "lower");
+
+      // get x_n
+      tmpxn = solve(tmpFn,tmpgn);
+      tmpdiagFn = diagvec(tmpFn);
+      s += 2*log(prod(tmpdiagFn)) - pow(sigma2,-2) * pow(norm(tmpxn,2),2)+ pow(sigma2,-1) * pow(norm((X.at(i)-H.at(i)*theta),2),2) + ni(i)*log(sigma2);
+
+
+    }
+  }else{
+    for (int i = 0; i < N ; i++) {
+
+      SA = sigma2list.at(i);
+      SA_sqrt = sqrt( 1/sigma2list.at(i));
+
+      tmp_cholM = IdentityMr + C.at(i).t()*BtAinvB.at(i)*C.at(i);
+      tmpgn = (C.at(i).t())*(BtAinvx.at(i) - BtAinvH.at(i)*theta);
+
+      // get F_n
+      tmpFn = chol(tmp_cholM, "lower");
+      // get x_n
+      tmpxn = solve(tmpFn,tmpgn);
+      tmpdiagFn = diagvec(tmpFn);
+      s += 2*log(prod(tmpdiagFn)) -  pow(norm(tmpxn,2),2) + pow(norm(((X.at(i)-H.at(i)*theta)%SA_sqrt),2),2);
+
+    }
+
+  }
+
+  // penalty
+  s += lambda_eigen_t * as_scalar(beta.t()*Omega_eigen_t*beta)+lambda_eigen_d * as_scalar(beta.t()*Omega_eigen_d*beta)+
+    lambda_mean_t * as_scalar(theta.t()*Omega_mean_t*theta)+lambda_mean_y * as_scalar(theta.t()*Omega_mean_y*theta);
+
+  return s;
+}
