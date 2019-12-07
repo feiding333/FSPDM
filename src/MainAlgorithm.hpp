a19f2b7824d4d835cb56b9b9dc8f728921e444d1
Fei Ding <feiding@tamu.edu>
2019-12-07 10:36:59 -0600

    Define set_data() function in MainAlgorithm class to set all input data.

--- src/MainAlgorithm.hpp
+++ src/MainAlgorithm.hpp
@@ -13,7 +13,20 @@ public:
     upsigma2 = upsigma20;
   }
 
-
+  // set data
+  void set_data(vector<vec> X_,
+                vector<vec> y_,
+                vector<mat> B_,
+                vector<mat> H_,
+                vec ni_,
+                mat Omega_eigen_t_,
+                mat Omega_eigen_d_,
+                mat Omega_mean_t_,
+                mat Omega_mean_y_,
+                double r_,
+                double k_,double lambda_eigen_t_,double lambda_eigen_d_,double lambda_mean_t_,double lambda_mean_y_,vec beta0_,vec theta0_,double sigma20_,double upsigma20_,vector<vec> sigma2list_){
+    set_data_raw (X_,y_,B_,H_,ni_,Omega_eigen_t_,Omega_eigen_d_,Omega_mean_t_,Omega_mean_y_,r_,k_,lambda_eigen_t_,lambda_eigen_d_,lambda_mean_t_,lambda_mean_y_,beta0_,theta0_,sigma20_,upsigma20_,sigma2list_);
+  }
 
 };
 
